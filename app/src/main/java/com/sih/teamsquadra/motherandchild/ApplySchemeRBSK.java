93fb3ecbd08a0bdd4e289e73145915b9add96be5
Vivekbarsaiyan94 <ee18mtech11028@iith.ac.in>
2019-03-02 23:58:41 +0530

    changes in govt scheme

---
+++ app/src/main/java/com/sih/teamsquadra/motherandchild/ApplySchemeRBSK.java
@@ -0,0 +1,13 @@
+package com.sih.teamsquadra.motherandchild;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ApplySchemeRBSK extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apply_scheme_rbsk);
+    }
+}
